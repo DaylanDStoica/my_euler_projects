a163c819fac68d9496df565ed61f8a291a764cd5
DaylanDStoica <daylanstoica@gmail.com>
2022-08-24 13:37:22 -0700

    first commit

---
+++ distinct_primes_factor.py
@@ -0,0 +1,25 @@
+
+# Daylan Stoica
+# @DaylanDStoica
+
+# 24 August 2022
+
+'''
+Distinct primes factors
+
+Project Eueler 
+Problem 47
+
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 * 7
+15 = 3 * 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2**2 * 7 * 23
+645 = 3 * 5 * 43
+646 = 2 * 17 * 19.
+
+Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
+'''
