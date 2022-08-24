449b09ab0d44d10f4c626a962ca1d43efd142251
DaylanDStoica <daylanstoica@gmail.com>
2022-08-24 14:17:14 -0700

    created the boolean for checking the number of distinct primes.

--- distinct_primes_factor.py
+++ distinct_primes_factor.py
@@ -23,3 +23,29 @@ The first three consecutive numbers to have three distinct prime factors are:
 
 Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
 '''
+
+from shared_math import build_prime_list
+def count_distinct_primes(number):
+    '''determine the number of distinct prime factors for the number
+    repeats do not trouble'''
+    prime_list = build_prime_list(number)
+    # print("prime list: ", prime_list)
+    distinct_primes = list(set(prime_list) )
+    # print("distinct primes", distinct_primes)
+    prime_count = len(distinct_primes)
+    # print("number of distinct primes  ", prime_count)
+    return prime_count
+
+def test_count_distinct():
+    for x in range(2, 25):
+        print("", x, " testing number of distinct primes")
+        count_distinct_primes(x) 
+
+# test_count_distinct()
+
+def is_correct_num_of_dist_prime_factors( number, count_distinct_primes = 4):
+    '''check if the given number has the correct number of distinct primes'''
+    return count_distinct_primes(number) == count_distinct_primes
+
+def check_consecutive_numbers(number, count_consecutives = 4, distinct_primes = 4):
+    pass
