f9eac477fec6fe7325b60db9fb932eeb7b14d147
DaylanDStoica <daylanstoica@gmail.com>
2022-08-25 16:33:38 -0700

    completed remaining functions for retrieving the starting number. figure out why '  number_of_distinct_primes = count_distinct_primes(number) TypeError: 'int' object is not callable '  occurs

--- distinct_primes_factor.py
+++ distinct_primes_factor.py
@@ -30,7 +30,10 @@ def count_distinct_primes(number):
     repeats do not trouble'''
     prime_list = build_prime_list(number)
     # print("prime list: ", prime_list)
-    distinct_primes = list(set(prime_list) )
+
+    # distinct_primes = list(set(prime_list) )
+    distinct_primes = set(prime_list)
+
     # print("distinct primes", distinct_primes)
     prime_count = len(distinct_primes)
     # print("number of distinct primes  ", prime_count)
@@ -45,7 +48,38 @@ def test_count_distinct():
 
 def is_correct_num_of_dist_prime_factors( number, count_distinct_primes = 4):
     '''check if the given number has the correct number of distinct primes'''
-    return count_distinct_primes(number) == count_distinct_primes
+    print("is_correct_num_of_dist_prime_factors (", number, ")")
+    # count_distinct_primes(number)
+    # if the number of distinct primes is equal to the desired amount, return True
+    number_of_distinct_primes = count_distinct_primes(number)
+    return ( number_of_distinct_primes == count_distinct_primes )
 
+print(is_correct_num_of_dist_prime_factors(6) )
 def check_consecutive_numbers(number, count_consecutives = 4, distinct_primes = 4):
-    pass
+    '''look for the first chain of 4 consecutive numbers with 4 prime factors'''
+    result = True
+    x = number
+    for i in range( count_consecutives):
+        if not is_correct_num_of_dist_prime_factors(x+i, distinct_primes):
+            result = False
+            break
+        else:
+            continue
+
+    return result
+
+def find_consecutives_start (count_consecutives = 4, distinct_primes = 4):
+    '''find the starting number that begins the consecutive postive integers chain'''
+    ret_num = 2
+    while not check_consecutive_numbers( count_consecutives, distinct_primes):
+        ret_num += 1
+    return ret_num
+
+def build_the_consecutives( start_num, count_consecutives = 4):
+    '''build the list of consecutive integers'''
+    ret_list = []
+    for i in range( count_conesecutives):
+        ret_list.append( start_num + i)
+    return ret_list
+
+print( "the starting number is : ", find_consecutives_start(4,4))
