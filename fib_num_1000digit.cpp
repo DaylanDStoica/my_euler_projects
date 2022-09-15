a7d6766bcb783d51e334970059c81ecdc0763bb6
DaylanDStoica <daylanstoica@gmail.com>
2022-09-15 12:02:06 -0700

    need to work on size manaement for variables. Values gets lost before digit-count is acquired

--- fib_num_1000digit.cpp
+++ fib_num_1000digit.cpp
@@ -32,3 +32,57 @@ The 12th term, F12, is the first term to contain three digits.
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
 */
+
+#include <iostream> 
+#include<string>
+using namespace std;
+
+
+int next_fib( int x1, int x2){
+    return x1 + x2 ;
+}
+/*
+void fib_sequence ( int* x1, int* x2, int* index){
+    int tempX = *x2;
+    cout << tempX << " test number pointer" << endl;
+    *x2 = next_fib( *x1, *x2);
+    *x1 = tempX;
+    *index++;
+
+    cout << "index: " << *index << "   " << *x1 << " , " << *x2 << endl;
+    
+}
+*/
+void fib_sequence ( double &x1, double &x2, int &index){
+    /*
+    int tempX = x2;
+    cout << tempX << " test number pointer" << endl;
+    x2 = next_fib( x1, x2);
+    x1 = tempX;
+    index++;
+
+    cout << "index: " << index << "   " << x1 << " , " << x2 << endl;
+    */
+    printf("before function:    index: %d, x1: %f, x2: %f \n", index, x1,x2);
+
+    double& temp = x2;
+    x2 = x1 + x2; 
+    x1 = temp;
+    index++;
+    printf("    after function:     index: %d, x1: %f, x2: %f \n", index, x1,x2);
+}
+
+
+int main(){
+    double num1 = 1;
+    double num2 = 1;
+    int index = 1;
+    while ( to_string(num2).length() <= 999){
+        // fib_sequence( &num1, &num2, &index);
+        fib_sequence(num1, num2, index);
+        cout << "number length " << to_string(num2).length();
+        // cin.ignore();
+    } 
+    //loop as long as the number has less than 1000 digits
+    return 0;
+}
