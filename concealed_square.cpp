3dcb187f6a6129024c5d2fcbf14440b9cd8b3f20
DaylanDStoica <daylanstoica@gmail.com>
2022-09-19 13:02:03 -0700

    first commit

---
+++ concealed_square.cpp
@@ -0,0 +1,20 @@
+
+// Daylan Stoica
+// @DaylanDStoica
+
+// 19 September 2022
+
+
+/*
+Concealed Square
+Problem 206
+
+Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
+where each “_” is a single digit.
+*/
+
+#include <iostream>
+
+int main(){
+    return 0;
+}
