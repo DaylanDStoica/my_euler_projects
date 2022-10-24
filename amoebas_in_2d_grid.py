f688034ac0eb4d1f5c69878a69ea735af5de399a
DaylanDStoica <daylanstoica@gmail.com>
2022-10-24 13:55:02 -0700

    built the amoeba class, and passed the test functions, both base-testcase and bulk testcase

--- amoebas_in_2d_grid.py
+++ amoebas_in_2d_grid.py
@@ -38,3 +38,33 @@ at the new spots, if the spots are already empty
 question: if the spaces are already filled, is the division not possible, 
     if yes, then the parent amoeba just disappears'''
 
+class Amoeba:
+    def __init__ ( self, pos_x = 0, pos_y = 0):
+        self.pos_x = pos_x 
+        self.pos_y = pos_y 
+        
+def divide_amoeba ( parent_amoeba : Amoeba):
+    # take in an Amoeba, and create two new of the class
+    # and drop the parent, 
+    # to represent the division 
+    parent_x = parent_amoeba.pos_x 
+    parent_y = parent_amoeba.pos_y 
+    
+    amoeba1 = Amoeba ( parent_x + 1, parent_y)
+    amoeba2 = Amoeba( parent_x + 1, ( parent_y + 1 ) % 4)
+    
+    return amoeba1, amoeba2
+
+def test_amoeba_class (starting_x = 0, starting_y = 0):
+    parent_amoeba = Amoeba( starting_x, starting_y )
+    print( " paraent coords: ( %d, %d )" %(parent_amoeba.pos_x, parent_amoeba.pos_y) )
+    amoeba1 , amoeba2 = divide_amoeba( parent_amoeba)
+    print( " children: ( %d , %d ) , ( %d, %d )" 
+        % (amoeba1.pos_x, amoeba1.pos_y, amoeba2.pos_x, amoeba2.pos_y) )
+    
+def loop_test_amoeba_class ():
+    for x in range ( 10):
+        for y in range( 23):
+            test_amoeba_class( x, y)
+            
+loop_test_amoeba_class()
