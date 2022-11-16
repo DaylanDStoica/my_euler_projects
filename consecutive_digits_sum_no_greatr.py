9bc9ae46b24b7a268b684e92dee2010183641f59
DaylanDStoica <daylanstoica@gmail.com>
2022-11-15 16:49:04 -0800

    first commit

---
+++ consecutive_digits_sum_no_greatr.py
@@ -0,0 +1,11 @@
+# Daylan Stoica
+# @DaylanDStoica
+# 15 November 2022
+
+'''
+Numbers for which no three consecutive digits have a sum greater than a given value 
+Problem 164
+
+How many 20 digit numbers n (without any leading zero) exist such that no three consecutive digits of n have a sum greater than 9?
+'''
+
