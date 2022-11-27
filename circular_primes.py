8b3b02614d4c86036a76703c74041f06b28cf9b0
DaylanDStoica <daylanstoica@gmail.com>
2022-11-26 19:28:31 -0800

    added use of iterations-package for permutations creation

--- circular_primes.py
+++ circular_primes.py
@@ -21,6 +21,7 @@ How many circular primes are there below one million?
 
 from shared_math import is_prime 
 
+import itertools #for getting the permutations
 def string_permuations( num):
     '''create the different permutations of a string's characters
     return list of the different ways the chars can be ordered'''
@@ -30,6 +31,15 @@ def string_permuations( num):
     # cycle through what the different permutations can be
     # startint with moving around the first index, and what can changed in the forward indices
     # and move forward from there.
+    # digit_list = str_num.break()
+    # digit_list = split( str_num)
+    digit_list = list(str_num)
+    
+    per_list = itertools.permutations( digit_list, len(digit_list) )
+    #get all the permutations, that are of the length of the original number
+    #AKA, rearrange the numbers, without dropping or replicating any digits
+    
+    # TODO: test the function, with the perm_list, and using iterations package.
     
     return perm_list
     
