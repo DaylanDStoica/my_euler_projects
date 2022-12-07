84ca453c5a23c01e43fe821b2bab67ea22fc4c48
DaylanDStoica <daylanstoica@gmail.com>
2022-12-06 16:32:21 -0800

    made a function to prove a perfect square, when it ends in one zero, it acutally ends in two zeros

--- con_square.py
+++ con_square.py
@@ -9,7 +9,6 @@ Problem 206
 
 Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
 where each “_” is a single digit.
-
 '''
 
 def square_is_valid (number):
@@ -49,3 +48,29 @@ def square_is_valid (number):
 
 
 # print ( square_is_valid ( 11221532040)  )
+
+# Note: because of how squares, with the final digit of a being 0
+# is likely for the second last digit of the square to also be 0
+# 10**2 = 100
+
+
+def is_last_digit_0( num):
+    # return a bool, 
+    # if the number ends in 0, return True
+    # if the number ends in any other number, return False 
+    return ( num%10 == 0)
+def proof_2_digits_zero():
+    # used to prove that the last 2 digits are zero, when the last digit is itself 0
+    count_non_double_zeros = 0
+    for x in range ( 1, 10000):
+        sq_num = x**2
+        if is_last_digit_0( sq_num):
+            # if the squares last digit is a 0
+            # print( sq_num, " is a square, ending in a 0", " with a base value of: ", x)
+            print( " with a base value of: ", x, " , the square that ends in a 0: ", sq_num)
+            if ( sq_num % 100 != 0):
+                # if the squares last 2 digits is not 0, but last digit is 0
+                count_non_double_zeros += 1
+                
+    print( " the number of squares that end in one zero but not two zeros: ", count_non_double_zeros)
+# proof_2_digits_zero()
