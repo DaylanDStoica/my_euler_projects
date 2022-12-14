298fd1732d88ecdccf00178289ec363b4eb6b162
DaylanDStoica <daylanstoica@gmail.com>
2022-12-14 15:44:50 -0800

    built reverse() and is_valid(), and base tested

--- count_reversible_numbers.py
+++ count_reversible_numbers.py
@@ -16,3 +16,31 @@ There are 120 reversible numbers below one-thousand.
 
 How many reversible numbers are there below one-billion (109)?
 '''
+
+def reverse(num):
+    ''' 
+    inverse the order of digits in the given number, and return the reversed order
+    decrease the digits to move forward'''
+    rev_num = 0
+    while num > 0:
+        # removing the right-most digit of num, move it to the right-most digt of rev_num
+        rev_num *= 10 # move the rev_num up a digit position
+        rev_num += num % 10 # get the right most digit 
+        num = num // 10 # remove the right most digit
+        
+    return rev_num 
+
+def is_valid(num):
+    '''if the sum of the number and reversed-number are odd, return True'''
+    reversed_sum = num + reverse(num)
+    if reversed_sum % 2 == 1 : # if is odd
+        return True 
+    else:
+        return False 
+    
+    
+def main():
+    # print ( 1023, reverse(1023) , 1023 + reverse(1023)) # test
+    pass
+    
+main()
