aef4298d11f67247d21126cfe159e737346ad46a
DaylanDStoica <daylanstoica@gmail.com>
2022-12-27 23:40:18 -0800

    began building the max_sum function

--- unsolved/matrix_sum.py
+++ unsolved/matrix_sum.py
@@ -5,16 +5,14 @@
 
 '''
 Matrix Sum
-
-
-  [Show HTML problem content]   [Show fastest solvers]  
 Problem 345
 
-We define the Matrix Sum of a matrix as the maximum possible sum of matrix elements such that none of the selected elements share the same row or column.
+We define the Matrix Sum of a matrix as the maximum possible sum of matrix elements 
+such that none of the selected elements share the same row or column.
 
 For example, the Matrix Sum of the matrix below equals 3315 ( = 863 + 383 + 343 + 959 + 767):
 [
-7  53 183 439 863
+  7  53 183 439 863
 497 383 563  79 973
 287  63 343 169 583
 627 343 773 959 943
@@ -41,3 +39,11 @@ Find the Matrix Sum of:
 813 883 451 509 615  77 281 613 459 205 380 274 302  35 805
 ]
 '''
+
+def find_the_highest_sum( matrix):
+    '''given a 2D amtrix of intgers, find the highest sum of elements
+    with unique columns and rows'''
+    sum = 0
+    
+    
+    return sum 
