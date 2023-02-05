8d368b8adb9514c0c49e20833cfeccb7cace1c25
DaylanDStoica <daylanstoica@gmail.com>
2023-02-04 20:21:59 -0800

    built the is_valid function, via splitting the sums to be checked. TODO: verify the integrity of the is_valid function.

--- count_reversible_numbers.py
+++ count_reversible_numbers.py
@@ -31,8 +31,8 @@ def reverse(num):
     return rev_num 
 
 def is_valid(num):
-    '''if the sum of the number and reversed-number are odd, return True
-    leading zeroes in either direction are unacceptable'''
+    '''if the sum of the number and reversed-number, has digits that are all odd, return True
+    leading zeroes in either direction are unacceptable/unconsidered'''
     
     # check for leading zeroes, that exist when reversed 
     if num % 10 == 0: # right-most digit is 0, that is a leading zero when flipped 
@@ -40,12 +40,29 @@ def is_valid(num):
     
     # check that the sum of [ num + reverse(num)] is odd
     reversed_sum = num + reverse(num)
-    if reversed_sum % 2 == 1 : # if is odd
-        return True 
-    else:
-        return False 
+    # print( num, reverse(num))
+    # print(" reversed sum ", reversed_sum)
+    # if reversed_sum % 2 == 1 : # if the sum is odd
+        # return True 
+    # else:
+        # return False 
     
+    # break up the digits of the sum into a list, for checking for non-odds
+    sum_digits = [] 
+    while reversed_sum > 0: 
+        sum_digits.append( reversed_sum%10) # add the right-most digit to the list 
+        reversed_sum = reversed_sum // 10  # drop the right-most digit 
 
+    # print( "list of sums: " , sum_digits)
+
+    result = True 
+    # go through the list of reversed sum_digits, checking for non-odds.
+    # immediately return False if even is encountered 
+    for x in sum_digits:
+        if x % 2 == 0: # the digit in the list is even, return False 
+            result = False 
+            break 
+# is_valid( 12237) # check the is_valid function successfully flips, sums, and booleans the given number
 def base_case_test():
     '''there are 120 reversible numbers before one-thousand
     check that 120 is returned'''
@@ -57,7 +74,7 @@ def base_case_test():
     print( " count of reversible numbers is: ", count)
     print("         count should be 120")
     
-base_case_test()
+# base_case_test()
 
 
 def main():
